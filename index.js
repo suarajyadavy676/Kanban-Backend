219c3f49a6ca126a3e1a1429454a1737afb11db9
Suraj Yadav <surajyadavy676@gmail.com>
2024-05-03 00:41:18 +0530

    adding signIn anfd register route

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express')
+const path = require('path')
 const dbConnection = require('./src/config/db')
+const userRouter = require('./src/routes/user.routes')
 const app = express()
 require('dotenv').config()
 
 let port = process.env.PORT || 4000
 
-app.use(express.json())
+// for ejs
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
 
+// for tailwind css
+app.use(express.static(path.join(__dirname,'public')));
+
+//middleware
+app.use(express.json())
 //home route
 app.get('/',(req,res)=>{
-  return res.send("<h1>This is home page</h1>")
+  return res.render("index",{name:"Home"})
 })
 
 // all routes
+app.use('/user',userRouter)
 
 app.listen(port,async()=>{
   try {
---
+++ src/routes/user.routes.js
@@ -0,0 +1,49 @@
+const {Router} = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const UserData = require('../models/user.model')
+
+let userRouter = Router()
+
+userRouter.post('/register',async(req,res)=>{
+  try {
+    const {email,password,username,role} = req.body
+    bcrypt.hash(password,10, async function(err, hash) {
+      // Store hash in your password DB.
+      if(err){
+        return res.status(400).send("hashing error")
+      }  
+      await UserData.create({username,email,password:hash,role})
+      return res.send("user register successfully")
+  });
+  } catch (error) {
+    return res.status(500).send("server error in register time")
+  }
+})
+
+userRouter.post('/signIn',async(req,res)=>{
+  let {email}=req.body
+  let user = await UserData.findOne({email})
+  let payload = {
+    name:user._id,
+    email:user.email
+  }
+  console.log(process.env.JWT_SECRET)
+  try {
+    if(!user){
+      return res.send("please register")
+    }else{
+      bcrypt.compare(req.body.password, user.password, function(err, result) {
+        if(err){
+          return res.send("wrong password")
+        }
+        let token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"1h"})
+        return res.send({token,messege:"Sign in successfully"})
+    })
+    }
+  } catch (error) {
+    return res.status(500).send("server error in signIn time")
+  }
+})
+
+module.exports = userRouter
